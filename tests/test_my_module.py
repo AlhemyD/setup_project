35dff5a6c66094a42455337397ccb08b8ba333b7
AlhemyD <138218446+AlhemyD@users.noreply.github.com>
2024-05-07 11:32:05 +0800

    Update test_my_module.py

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,8 +1,4 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
-
-from my_module import add
+from src.my_module import add
 
 def test_add():
     assert add(2, 3) == 5
